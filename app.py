b5abc0613030f0d94acde98dd4ef8fa47cf917b1
Rajayne <ryann.j.nealey@gmail.com>
2023-03-02 16:39:25 -0800

    Create functional restart button

--- app.py
+++ app.py
@@ -17,8 +17,12 @@ debug = DebugToolbarExtension(app)
 def show_board():
     board = boggle_game.make_board()
     session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
 
-    return render_template('boggle.html', board=board)
+    return render_template('boggle.html', board=board, 
+                        highscore=highscore,
+                        nplays=nplays)
 
 @app.route('/check-word')
 def check_word():
@@ -27,3 +31,20 @@ def check_word():
     response = boggle_game.check_valid_word(board, word)
 
     return jsonify({'result': response})
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
+
+@app.route('/restart')
+def restart_game():
+    return redirect('/')
